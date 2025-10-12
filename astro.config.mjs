f834a9a40d84fe399f1e21bad84a3d5233d8dd2d
Antonio Kobashikawa <akobashikawa@gmail.com>
2025-10-11 23:42:40 -0500

    Add soporte react, vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,31 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import vue from '@astrojs/vue';
+
+// import rehypeMermaid from 'rehype-mermaid';
+// import remarkMath from 'remark-math';
+// import rehypeKatex from 'rehype-katex';
+// import remarkObsidianLinks from './src/plugins/remark-obsidian-links';
+
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://loqueaprendihoy.akcstudio.com',
+    integrations: [mdx(), sitemap(), react(), vue()],
+    markdown: {
+        syntaxHighlight: {
+            type: 'shiki',
+            excludeLangs: ['mermaid', 'math'],
+        },
+        // rehypePlugins: [
+        // 	rehypeMermaid,
+        // 	rehypeKatex
+        // ],
+        // remarkPlugins: [
+        // 	remarkObsidianLinks,
+        // 	remarkMath
+        // ]
+    }
 });
